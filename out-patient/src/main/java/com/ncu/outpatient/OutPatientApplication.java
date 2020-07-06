be9e9fcf241fc93c7316e7049e1b03786322afec
ITBoy-China <vicbelieve@outlook.com>
2020-07-06 20:16:35 +0800

    init 项目初始化

---
+++ out-patient/src/main/java/com/ncu/outpatient/OutPatientApplication.java
@@ -0,0 +1,13 @@
+package com.ncu.outpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutPatientApplication.class, args);
+    }
+
+}
---
+++ register-center/src/test/java/com/ncu/registercenter/RegisterCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncu.registercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
