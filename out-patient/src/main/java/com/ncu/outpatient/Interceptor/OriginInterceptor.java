aac6d0c70c56fc4d0ea7a48744b319dc45660ce8
ITBoy-China <vicbelieve@outlook.com>
2020-07-12 17:55:19 +0800

    feat(gateway out-patient)不允许不通过gateway访问单个服务

---
+++ out-patient/src/main/java/com/ncu/outpatient/Interceptor/OriginInterceptor.java
@@ -0,0 +1,30 @@
+package com.ncu.outpatient.Interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author : 城南有梦
+ * @date : 2020-07-12 17:06:36
+ * @description:
+ * 自定义拦截器 拦截不是来自网关的
+ */
+@Component
+public class OriginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取头部信息
+        String origin = request.getHeader("from");
+        if(origin==null){
+            return false;
+        }else if(origin.equals("gateway")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
